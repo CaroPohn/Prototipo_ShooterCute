6f3082dd6a88d1cca07e840856c0155a10e48909
FabriLM98 <63129784+FabriLM98@users.noreply.github.com>
2025-06-07 00:40:33 -0300

    Added effect when hit to enemy

--- Assets/FABRI/HitEffectController.cs
+++ Assets/FABRI/HitEffectController.cs
@@ -4,72 +4,86 @@ using System.Collections;
 
 public class HitEffectController : MonoBehaviour
 {
-    [Range(0f, 1f)]
-    public float hitAmount = 0f;
+    //[Range(0f, 1f)]
+    float hitAmount = 0f;
 
-    private Renderer rend;
+    [SerializeField] private Renderer[] renderers;
+    public float effectDuration = 0.4f;
     private MaterialPropertyBlock block;
     private bool isPlayerInside = false;
     private Coroutine resetRoutine;
+    [ColorUsageAttribute(true, true)] [SerializeField] Color originalColor;
+    bool colorWasChanged = false;
 
     void Start()
     {
-        rend = GetComponent<Renderer>();
+        //rend = GetComponent<Renderer>();
         block = new MaterialPropertyBlock();
-        UpdateHitAmount();
+        
+        //UpdateHitAmount();
     }
 
-    public void UpdateHitAmount()
+    void UpdateHitAmount()
     {
-        rend.GetPropertyBlock(block);
-        block.SetFloat("_HitAmount", hitAmount);
-        rend.SetPropertyBlock(block);
+        foreach (Renderer rend in renderers) 
+        {
+            rend.GetPropertyBlock(block);
+            block.SetFloat("_HitAmount", hitAmount);
+            rend.SetPropertyBlock(block);
+        }
     }
 
     void Update()
     {
-        if (isPlayerInside && Input.GetKeyDown(KeyCode.F))  // ← CAMBIO AQUÍ
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (resetRoutine != null)
-                StopCoroutine(resetRoutine);
-
-            resetRoutine = StartCoroutine(HitEffect());
+            GetHit();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GetHit(Color.green);
         }
     }
-
+    public void GetHit()
+    {
+        if (resetRoutine != null) 
+        {
+            StopCoroutine(resetRoutine);
+        }
+        resetRoutine = StartCoroutine(HitEffect());
+    }
+    public void GetHit(Color color)
+    {
+        ChangeRenderersColors(color);
+        colorWasChanged = true;
+        GetHit();
+    }
+    void ChangeRenderersColors(Color color)
+    {
+        foreach (Renderer rend in renderers)
+        {
+            rend.GetPropertyBlock(block);
+            block.SetColor("_Color", color);
+            rend.SetPropertyBlock(block);
+        }
+    }
     IEnumerator HitEffect()
     {
         hitAmount = 1f;
         UpdateHitAmount();
-
-        float duration = 0.4f;
         float timer = 0f;
 
-        while (timer < duration)
+        while (timer < effectDuration)
         {
             timer += Time.deltaTime;
-            hitAmount = Mathf.Lerp(1f, 0f, timer / duration);
+            hitAmount = Mathf.Lerp(1f, 0f, timer / effectDuration);
             UpdateHitAmount();
             yield return null;
         }
 
         hitAmount = 0f;
         UpdateHitAmount();
+        if (colorWasChanged) ChangeRenderersColors(originalColor);
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerInside = true;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerInside = false;
-        }
-    }
 }
