0e681d67721f1736b431d8eb8489bb70213cd9b9
nachosou <nachosou123@gmail.com>
2025-06-21 17:08:58 -0300

    Add lava damage X time after exiting lava

--- Assets/Scripts/DeathMapLimit.cs
+++ Assets/Scripts/DeathMapLimit.cs
@@ -1,8 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class DeathMapLimit : MonoBehaviour
 {
     public float damage = 0.2f;
+    public float postExitDuration = 3f;
+    public float damageInterval = 0.1f;
+
+    private float timeElapsedSinceExit;
+
+    private void Update()
+    {
+        timeElapsedSinceExit += Time.deltaTime;
+    }
 
     private void OnCollisionStay(Collision collision)
     {
@@ -15,6 +25,34 @@ public class DeathMapLimit : MonoBehaviour
                 playerHealth.TakeDamage(damage);
                 playerHealth.SetDamageType(PlayerHealthSystem.DamageType.Lava);
             }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Player salió del DeathMapLimit");
+
+            PlayerHealthSystem playerHealth = collision.transform.GetComponent<PlayerHealthSystem>();
+
+            if (playerHealth != null)
+            {
+                StartCoroutine(DamageOverTimeAfterExit(playerHealth));
+            }
+        }
+    }
+
+    private IEnumerator DamageOverTimeAfterExit(PlayerHealthSystem playerHealth)
+    {
+        timeElapsedSinceExit = 0;
+
+        while (timeElapsedSinceExit < postExitDuration)
+        {
+            playerHealth.TakeDamage(damage);
+            playerHealth.SetDamageType(PlayerHealthSystem.DamageType.Lava);
+
+            yield return new WaitForSeconds(damageInterval);
         }
     }
 }
