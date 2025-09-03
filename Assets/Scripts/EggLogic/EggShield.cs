9f218625185483fcdd6d0433cc3a07a056eb747e
FabriLM98 <fabrizzio.morandi@estudiante.imagecampus.edu.ar>
2025-09-03 14:22:11 -0300

    Egg shield now has functionality to appear, desintegrate and get hit

---
+++ Assets/Scripts/EggLogic/EggShield.cs
@@ -0,0 +1,102 @@
+using NUnit.Framework.Internal;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.Rendering;
+
+public class EggShield : MonoBehaviour
+{
+
+    [SerializeField] List<Renderer> corruptedEggRenderers;
+    [SerializeField] float timeToDesintegrate;
+    [SerializeField] float timeToAppear;
+    [SerializeField] float maxGlowWhenHit;
+    [SerializeField] float timeForGlowToFadeOut;
+    Coroutine eggGetHitCoroutine;
+    private MaterialPropertyBlock block;
+
+    private void Start()
+    {
+        block = new MaterialPropertyBlock();
+    }
+
+    void UpdateRenderersPropertyValue(List<Renderer> renderersList,float value, string propertyName)
+    {
+        foreach (Renderer renderer in renderersList)
+        {
+            renderer.GetPropertyBlock(block);
+            block.SetFloat(propertyName, value);
+            renderer.SetPropertyBlock(block);
+        }
+    }
+
+    public void UpdateEggFadeProgress(float progress)
+    {
+        UpdateRenderersPropertyValue(corruptedEggRenderers, progress,"_Desintegration");
+    }
+
+    public void UpdateShieldHitGlow(float damage)
+    {
+        UpdateRenderersPropertyValue(corruptedEggRenderers, damage, "_Blend_Color_Opacity");
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetHit();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Appear();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Desintegrate();
+        }
+    }
+    public void Desintegrate()
+    {
+        StartCoroutine("DesintegrateCoroutine");
+    }
+    public void Appear()
+    {
+        StartCoroutine("AppearCoroutine");
+    }
+
+    public void GetHit()
+    {
+        if (eggGetHitCoroutine != null) StopCoroutine(eggGetHitCoroutine);
+        eggGetHitCoroutine = StartCoroutine("ShieldGetHitCoroutine");
+    }
+    IEnumerator ShieldGetHitCoroutine()
+    {
+        UpdateShieldHitGlow(maxGlowWhenHit);
+        for (float f = 0; f < timeForGlowToFadeOut; f += Time.deltaTime)
+        {
+            UpdateShieldHitGlow(maxGlowWhenHit - (f / timeForGlowToFadeOut)* maxGlowWhenHit);
+            yield return null;
+        }
+        UpdateShieldHitGlow(0);
+    }
+        IEnumerator DesintegrateCoroutine()
+    {
+        for (float f = 0; f < timeToDesintegrate; f += Time.deltaTime)
+        {
+            UpdateEggFadeProgress(f / timeToDesintegrate);
+            yield return null;
+        }
+        UpdateEggFadeProgress(1f);
+    }
+    IEnumerator AppearCoroutine()
+    {
+        for (float f = 0; f < timeToAppear; f += Time.deltaTime)
+        {
+            UpdateEggFadeProgress(1 - (f / timeToAppear));
+            yield return null;
+        }
+        UpdateEggFadeProgress(0f);
+    }
+}
