66279b15639cabeed6fb934b58482335711ab243
FabriLM98 <fabrizzio.morandi@estudiante.imagecampus.edu.ar>
2025-10-12 18:00:02 -0300

    Added animations for ability cooldown

--- Assets/Art/TestArt.cs
+++ Assets/Art/TestArt.cs
@@ -1,19 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestArt : MonoBehaviour
 {
-    [SerializeField] StationWithEggEffects stationWithEggEffects;
+    [SerializeField] Animator anim;
+    [SerializeField] Image image;
+    [SerializeField] float cooldown = 3;
+    float fill = 1f;
+    bool onCooldown = false;
+    float timer = 0;
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.C))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            stationWithEggEffects.Close();
+            if (anim.GetBool("isReady"))
+            {
+                fill = 0f;
+                timer = 0;
+                onCooldown = true;
+            }
+            else 
+            {
+                fill = 1f;
+                onCooldown = false;
+            }
+            anim.SetBool("isReady",!anim.GetBool("isReady"));
         }
-        else if (Input.GetKeyUp(KeyCode.D))
+        if (!anim.GetBool("isReady"))
         {
-            stationWithEggEffects.Die();
+            timer += Time.deltaTime;
+            fill = timer/cooldown;
         }
-        
+        image.fillAmount = Mathf.Clamp(fill,0,1);
     }
 }
