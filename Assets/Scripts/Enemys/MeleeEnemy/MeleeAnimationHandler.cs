3151f7f04a447eba2248597312d983fdff3ab5a1
nachosou <nachosou123@gmail.com>
2025-10-15 17:52:46 -0300

    Add melee enemys FSM

---
+++ Assets/Scripts/Enemys/MeleeEnemy/MeleeAnimationHandler.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+public class MeleeAnimationHandler : MonoBehaviour
+{
+    private MeleeEnemy meleeEnemy;
+
+    public event Action OnEnemyShooting;
+
+    private void Start()
+    {
+        meleeEnemy = GetComponentInParent<MeleeEnemy>();
+    }
+
+    public void AttackPoseReached()
+    {
+        OnEnemyShooting?.Invoke();
+    }
+
+    public void OnFinishDeadAnimation()
+    {
+        meleeEnemy.stopMeleeDieAnimation = true;
+    }
+
+    public void OnFinishSpawnAnimation()
+    {
+        meleeEnemy.stopMeleeSpawnAnimation = true;
+    }
+}
---
+++ Assets/Scripts/Enemys/MeleeEnemy/MeleeEnemy.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private Animator enemyAnimator;
+
+    [Header("Follow")]
+    private Transform player;
+
+    public float attackDistance;
+
+    [Header("Shoot")]
+    [SerializeField] private Transform shootPoint;
+
+    private List<Collider> enemyColliders;
+
+    [SerializeField] private MeleeAnimationHandler meleeAnimationHandler;
+
+    [SerializeField] private GameObject spawnVFX;
+
+    public float attackCoolDown;
+    public float damage;
+    public float attackTimer;
+
+    public bool stopMeleeDieAnimation;
+    public bool stopMeleeSpawnAnimation;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        enemyColliders = new List<Collider>(GetComponentsInChildren<Collider>());
+
+        agent = GetComponent<NavMeshAgent>();
+
+        stopMeleeDieAnimation = false;
+        stopMeleeSpawnAnimation = false;
+    }
+
+    private void OnEnable()
+    {
+        meleeAnimationHandler.OnEnemyShooting += MeleeLogic;
+    }
+
+    private void OnDisable()
+    {
+        meleeAnimationHandler.OnEnemyShooting -= MeleeLogic;
+    }
+
+    public void MeleeLogic()
+    {
+        
+    }
+
+    public void DeactivateColliders()
+    {
+        foreach (Collider collider in enemyColliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    public bool IsPlayerOnRange()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        return distanceToPlayer <= attackDistance;
+    }
+
+    public void SetTargetToFollow()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    public void StopFollowingPlayer(bool doesEnemyhasToStop)
+    {
+        agent.isStopped = doesEnemyhasToStop;
+
+        if (!doesEnemyhasToStop)
+        {
+            SetTargetToFollow();
+        }
+
+        if (!agent.isStopped)
+        {
+            enemyAnimator.SetFloat("Velocity", 0.5f);
+        }
+        else
+        {
+            enemyAnimator.SetFloat("Velocity", 0.0f);
+        }
+    }
+
+    public void SetLookAt()
+    {
+        Vector3 vec1 = transform.position;
+        Vector3 vec2 = player.position;
+
+        Vector3 vecLookAt = vec2 - vec1;
+        vecLookAt.y = 0f;
+
+        if (vecLookAt != Vector3.zero)
+        {
+            transform.forward = vecLookAt.normalized;
+        }
+    }
+
+    public void SetHealthSystemActive(HealthMeleeSystem healthSystem, bool isActive)
+    {
+        healthSystem.enabled = isActive;
+    }
+
+    public void AttackAnimationHandler()
+    {
+        enemyAnimator.SetTrigger("Attack");
+    }
+
+    public void SpawnAnimationHandler()
+    {
+        StartCoroutine(SpawnCoroutine());
+
+        Instantiate(spawnVFX, transform.position, transform.rotation);
+    }
+
+    public void DieAnimationHandler()
+    {
+        StartCoroutine(DieCoroutine());
+    }
+
+    public IEnumerator DieCoroutine()
+    {
+        while (!stopMeleeDieAnimation)
+        {
+            enemyAnimator.SetTrigger("Die");
+
+            yield return null;
+        }
+    }
+
+    public IEnumerator SpawnCoroutine()
+    {
+        enemyAnimator.SetTrigger("Spawn");
+
+        yield return null;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+    }
+}
