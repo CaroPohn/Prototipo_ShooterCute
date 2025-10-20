1a9a31d571ea53887ba658ca8273536605c267d6
nachosou <nachosou123@gmail.com>
2025-10-20 15:59:28 -0300

    Fix guns vfx moving with gun

--- Assets/RotGuns.cs
+++ Assets/RotGuns.cs
@@ -4,7 +4,7 @@ public class RotGuns : MonoBehaviour
 {
     [SerializeField] Transform cameraOrientation;
 
-    private void Start()
+    private void Update()
     {
         Quaternion targetRotation = Quaternion.Euler(cameraOrientation.eulerAngles.x, cameraOrientation.eulerAngles.y, 0);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1);
