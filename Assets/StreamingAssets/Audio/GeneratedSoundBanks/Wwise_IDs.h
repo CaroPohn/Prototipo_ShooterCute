396d0fcfc6be2d09f4317de6d943fcd75a3ca3c3
heinzenknecht <guidohkt@gmail.com>
2025-10-28 20:19:20 -0300

    Change UI_Slider to short one-shot

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -53,8 +53,7 @@ namespace AK
         static const AkUniqueID PROJECTILE_SOUL_DEPLOY = 2994932292U;
         static const AkUniqueID UI_BUTTON_NORMAL = 3345914708U;
         static const AkUniqueID UI_BUTTON_SPECIAL = 3115620430U;
-        static const AkUniqueID UI_SLIDER_HOLD = 634443763U;
-        static const AkUniqueID UI_SLIDER_RELEASE = 1205364051U;
+        static const AkUniqueID UI_SLIDER = 3987036369U;
     } // namespace EVENTS
 
     namespace STATES
