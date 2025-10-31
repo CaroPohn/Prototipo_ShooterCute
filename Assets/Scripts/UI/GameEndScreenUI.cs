b8c7a254968855464b73d0a486155aa5335cf8fc
FabriLM98 <fabrizzio.morandi@estudiante.imagecampus.edu.ar>
2025-10-31 14:07:59 -0300

    Fixed a bug to avoid overlapping buttons in game over and accomplished screens

--- Assets/Scripts/UI/GameEndScreenUI.cs
+++ Assets/Scripts/UI/GameEndScreenUI.cs
@@ -4,15 +4,19 @@ public class GameEndScreenUI : MonoBehaviour
 {
     [SerializeField] Animator anim;
     [SerializeField] GameObject containerGO;
+    [SerializeField] GameObject missionFailedContainer;
+    [SerializeField] GameObject missionAccomplishedContainer;
     
     public void PlayMissionFailedAnimation()
     {
         containerGO.SetActive(true);
         anim.SetTrigger("Failed");
+        missionFailedContainer.SetActive(true);
     }
     public void PlayMissionAccomplishedAnimation()
     {
         containerGO.SetActive(true);
         anim.SetTrigger("Accomplished");
+        missionAccomplishedContainer.SetActive(true);
     }
 }
