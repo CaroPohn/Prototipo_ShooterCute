95f8364571ec640a79bedc412f21b4d2091c03c0
FabriLM98 <fabrizzio.morandi@estudiante.imagecampus.edu.ar>
2025-11-03 21:07:10 -0300

    Added gun color change according to Lumming selected

--- Assets/Scripts/LummingOnTable.cs
+++ Assets/Scripts/LummingOnTable.cs
@@ -4,23 +4,30 @@ public class LummingOnTable : MonoBehaviour
 {
     [SerializeField] GameObject bombGO;
     [SerializeField] GameObject chispeanGO;
+    [SerializeField] Renderer gunRenderer;
+    [SerializeField] Material bombMat;
+    [SerializeField] Material chispeanMat;
+    [SerializeField] Material gunDefaultMaterial;
     public void UpdateLummingOnTable(Lumming lumming)
     {
         if(lumming == Lumming.Bomb)
         {
             chispeanGO.SetActive(false);
             bombGO.SetActive(true);
+            gunRenderer.material = bombMat;
 
         }
         else if(lumming == Lumming.Chispean)
         {
             chispeanGO.SetActive(true);
             bombGO.SetActive(false);
+            gunRenderer.material = chispeanMat;
         }
         else
         {
             chispeanGO.SetActive(false);
             bombGO.SetActive(false);
+            gunRenderer.material = gunDefaultMaterial;
 
         }
     }
