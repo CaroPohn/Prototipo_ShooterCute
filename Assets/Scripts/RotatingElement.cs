1a38ece545a6980a2d3fac6fb5755e37d546307f
nachosou <nachosou123@gmail.com>
2025-11-17 01:06:29 -0300

    Add rotating interactable gun in space ship

--- Assets/Scripts/RotatingElement.cs
+++ Assets/Scripts/RotatingElement.cs
@@ -2,10 +2,42 @@ using UnityEngine;
 
 public class RotatingElement : MonoBehaviour
 {
-    [SerializeField] float rotatingSpeed = 1;
-    
+    [SerializeField] float autoRotateSpeed = 20f;
+    [SerializeField] float dragRotateSpeed = 5f;
+
+    private bool isDragging = false;
+    private float lastMouseX;
+
     void Update()
     {
-        transform.Rotate(new Vector3 (0, rotatingSpeed * Time.deltaTime, 0));
+        HandleMouseDrag();
+
+        if (!isDragging)
+        {
+            transform.Rotate(Vector3.up * autoRotateSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+    void HandleMouseDrag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMouseX = Input.mousePosition.x;
+            isDragging = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            float deltaX = Input.mousePosition.x - lastMouseX;
+
+            transform.Rotate(Vector3.up, -deltaX * dragRotateSpeed, Space.World);
+
+            lastMouseX = Input.mousePosition.x;
+        }
     }
 }
