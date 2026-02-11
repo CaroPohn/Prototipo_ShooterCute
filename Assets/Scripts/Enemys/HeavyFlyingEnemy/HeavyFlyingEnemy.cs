6bb0c63756641d91faebefdf26b0acf4cea0edc2
nachosou <nachosou123@gmail.com>
2026-02-11 17:00:55 -0300

    Add flying nav mesh

--- Assets/Scripts/Enemys/HeavyFlyingEnemy/HeavyFlyingEnemy.cs
+++ Assets/Scripts/Enemys/HeavyFlyingEnemy/HeavyFlyingEnemy.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HeavyFlyingEnemy : MonoBehaviour
 {
-    
+    private NavMeshAgent agent;
+    private Transform target;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float flyingHeight;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        agent.baseOffset = flyingHeight;
+        agent.speed = moveSpeed;
+    }
+
+    private void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        if (target != null) 
+        { 
+            agent.destination = target.position;
+        }
+    }
 }
